a5113bb40d522c6dcfb24f8427b7130bcf43f379
maglom <magnuslomsdalen@gmail.com>
2022-09-05 15:40:52 +0200

    ny scrape

--- scrape/riesling/script.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('riesling.csv', 'a')as file1:
-    writer = csv.DictWriter(file1)
-    with open('riesling1.csv', 'r')as file2:
-        reader = csv.DictReader(file2)
-        for i in reader:
-            writer.writerow(i)
