95bfc5865f6b62cd77964883c8cf4a72452f685e
maglom <magnuslomsdalen@gmail.com>
2022-09-06 15:25:02 +0200

    stuff

--- weather.py
+++ weather.py
@@ -1,15 +1,15 @@
 from datetime import datetime
-import matplotlib.pyplot as plt
 import meteostat
 import requests
+import pandas as pd
 
 
-def get_weather(region, year):
+def get_weather(location, year):
     start = datetime(year, 4, 1)
     end = datetime(year, 9, 30)
 
     respons = requests.get(
-        f'https://api.opencagedata.com/geocode/v1/json?q={region}&key=555c1da178f94aa4897b0393225d0776&language=en&pretty=1')
+        f'https://api.opencagedata.com/geocode/v1/json?q={location}&key=555c1da178f94aa4897b0393225d0776&language=en&pretty=1')
     response = respons.json()
 
     longlat = meteostat.Point(
@@ -17,12 +17,10 @@ def get_weather(region, year):
 
     data = meteostat.Monthly(loc=longlat, start=start, end=end)
     data = data.fetch()
-
     df = data.iloc[:, :4]
     df = df.stack().reset_index()
     df = df.T
     return df
 
-
-df = get_weather('bordeaux')
+get_weather('Pauillac', 2020)
 
