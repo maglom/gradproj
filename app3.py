e4e5abd774be9373e02038a4abbab224818c1934
Maglom <magnuslomsdalen@gmail.com>
2022-09-14 13:24:21 +0200

    laget ny værfunksjon

--- app3.py
+++ app3.py
@@ -3,21 +3,41 @@ from flask import Flask, request, jsonify, render_template, url_for, redirect
 from sql.sqlfunc import query
 from tabulate import tabulate
 
-#wines = query('select distinct wine from viners order by wine asc')
-#winelist = [[''.join(x)] for x in wines]
-#years = query('select distinct years from viners order by years asc')
-#yearlist = [[''.join(x)] for x in years]
+wines = ['Petrus', "Baron Philippe de Rothschild Chateau d'Armailhac","Chateau Angelus","Chateau Ausone","Chateau Batailley Grand Cru",
+"Chateau Bellevue-Mondotte","Chateau Beychevelle","Chateau Branaire-Ducru","Chateau Brane-Cantenac","Chateau Calon-Segur","Chateau Canon",
+"Chateau Cantemerle","Chateau Cheval Blanc","Chateau Clerc-Milon","Chateau Climens","Chateau Clinet","Chateau Cos d'Estournel","Chateau Cos d'Estournel Blanc",
+"Chateau d'Yquem","Chateau de Fargues","Chateau Doisy-Vedrines","Chateau Ducru-Beaucaillou","Chateau Duhart-Milon","Chateau Faugeres","Chateau Figeac",
+"Chateau Giscours","Chateau Giscours La Sirene de Giscours","Chateau Grand-Puy-Lacoste","Chateau Gruaud-Larose","Chateau Gruaud-Larose 'Sarget de Gruaud-Larose'",
+"Chateau Haut-Bailly","Chateau Haut-Batailley","Chateau Haut-Brion","Chateau L'Eglise-Clinet","Chateau La Conseillante","Chateau La Fleur-Petrus",
+"Chateau La Mission Haut-Brion","Chateau Lafite Rothschild","Chateau Lafite Rothschild 'Carruades de Lafite'","Chateau Lafleur","Chateau Lagrange","Chateau Lascombes",
+"Chateau Latour","Chateau Latour-Martillac","Chateau Latour 'Les Forts de Latour'","Chateau Leoville-Las Cases 'Clos du Marquis'","Chateau Leoville-Las Cases 'Grand Vin de Leoville'",
+"Chateau Leoville Barton","Chateau Leoville Poyferre","Chateau Lynch-Bages","Chateau Margaux","Chateau Montrose","Chateau Mouton Rothschild","Chateau Nenin","Chateau Palmer",
+"Chateau Pape Clement","Chateau Pavie","Chateau Pichon-Longueville au Baron de Pichon-Longueville","Chateau Pichon Longueville Comtesse de Lalande","Chateau Pontet-Canet",
+"Chateau Rauzan-Segla","Chateau Smith Haut Lafitte","Chateau Suduiraut","Chateau Talbot","Chateau Troplong Mondot","Chateau Trotanoy","Chateau Trotte Vieille",
+"Denis Dubourdieu Chateau Doisy-Daene L'Extravagant de Doisy-Daene","Domaine de Chevalier","Donjon de Lamarque","La Clarte de Haut-Brion Blanc","Le Clarence de Haut-Brion - Chateau Bahans Haut-Brion",
+"Le Pin","Pavillon Blanc du Chateau Margaux","Pavillon Rouge du Chateau Margaux","vieux Chateau Certan"]
+years = ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022"]
 
-#tablewine = tabulate(winelist, tablefmt='html')
-#tableyear = tabulate(yearlist, tablefmt='html')
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    wines = query('select distinct wine from viners order by wine asc')
-    years = query('select distinct years from viners order by years asc')
-    return render.template('wine.html', wines=wines, years=years)
+    wines = ['Petrus', "Baron Philippe de Rothschild Chateau d'Armailhac","Chateau Angelus","Chateau Ausone","Chateau Batailley Grand Cru",
+    "Chateau Bellevue-Mondotte","Chateau Beychevelle","Chateau Branaire-Ducru","Chateau Brane-Cantenac","Chateau Calon-Segur","Chateau Canon",
+    "Chateau Cantemerle","Chateau Cheval Blanc","Chateau Clerc-Milon","Chateau Climens","Chateau Clinet","Chateau Cos d'Estournel","Chateau Cos d'Estournel Blanc",
+    "Chateau d'Yquem","Chateau de Fargues","Chateau Doisy-Vedrines","Chateau Ducru-Beaucaillou","Chateau Duhart-Milon","Chateau Faugeres","Chateau Figeac",
+    "Chateau Giscours","Chateau Giscours La Sirene de Giscours","Chateau Grand-Puy-Lacoste","Chateau Gruaud-Larose","Chateau Gruaud-Larose 'Sarget de Gruaud-Larose'",
+    "Chateau Haut-Bailly","Chateau Haut-Batailley","Chateau Haut-Brion","Chateau L'Eglise-Clinet","Chateau La Conseillante","Chateau La Fleur-Petrus",
+    "Chateau La Mission Haut-Brion","Chateau Lafite Rothschild","Chateau Lafite Rothschild 'Carruades de Lafite'","Chateau Lafleur","Chateau Lagrange","Chateau Lascombes",
+    "Chateau Latour","Chateau Latour-Martillac","Chateau Latour 'Les Forts de Latour'","Chateau Leoville-Las Cases 'Clos du Marquis'","Chateau Leoville-Las Cases 'Grand Vin de Leoville'",
+    "Chateau Leoville Barton","Chateau Leoville Poyferre","Chateau Lynch-Bages","Chateau Margaux","Chateau Montrose","Chateau Mouton Rothschild","Chateau Nenin","Chateau Palmer",
+    "Chateau Pape Clement","Chateau Pavie","Chateau Pichon-Longueville au Baron de Pichon-Longueville","Chateau Pichon Longueville Comtesse de Lalande","Chateau Pontet-Canet",
+    "Chateau Rauzan-Segla","Chateau Smith Haut Lafitte","Chateau Suduiraut","Chateau Talbot","Chateau Troplong Mondot","Chateau Trotanoy","Chateau Trotte Vieille",
+    "Denis Dubourdieu Chateau Doisy-Daene L'Extravagant de Doisy-Daene","Domaine de Chevalier","Donjon de Lamarque","La Clarte de Haut-Brion Blanc","Le Clarence de Haut-Brion - Chateau Bahans Haut-Brion",
+    "Le Pin","Pavillon Blanc du Chateau Margaux","Pavillon Rouge du Chateau Margaux","vieux Chateau Certan"]
+    years = ["2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022"]
+    return render_template('wine.html', wines=wines, years=years)
     
     #if request.method == 'POST':
     #    wine = request.form['wine']
--- weather/weather.py
+++ weather/weather.py
@@ -1,18 +1,23 @@
 from datetime import datetime
 import meteostat
 import requests
+import pandas as pd
 
 
-def get_weather(location, year):
+def get_weather_monthly(location, year):
     start = datetime(year, 4, 1)
     end = datetime(year, 9, 30)
 
-    respons = requests.get(
-        f'https://api.opencagedata.com/geocode/v1/json?q={location}&key=555c1da178f94aa4897b0393225d0776&language=en&pretty=1',timeout=10)
-    response = respons.json()
+    # respons = requests.get(
+    #     f'https://api.opencagedata.com/geocode/v1/json?q={location}&key=555c1da178f94aa4897b0393225d0776&language=en&pretty=1',timeout=10)
+    # response = respons.json()
 
+    # longlat = meteostat.Point(
+    #     response['results'][1]['geometry']['lat'], response['results'][1]['geometry']['lng'])
+    
     longlat = meteostat.Point(
-        response['results'][1]['geometry']['lat'], response['results'][1]['geometry']['lng'])
+        44.864812831248145, -0.5748540613726731
+        )
 
     data = meteostat.Monthly(loc=longlat, start=start, end=end)
     data = data.fetch()
@@ -21,4 +26,56 @@ def get_weather(location, year):
     df = df.T
     return df
 
+
+def get_weather_daily(location, year):
+    start = datetime(year, 4, 1)
+    end = datetime(year, 9, 30)
+
+    # respons = requests.get(
+    #     f'https://api.opencagedata.com/geocode/v1/json?q={location}&key=555c1da178f94aa4897b0393225d0776&language=en&pretty=1',timeout=10)
+    # response = respons.json()
+
+    # longlat = meteostat.Point(
+    #     response['results'][1]['geometry']['lat'], response['results'][1]['geometry']['lng'])
+    
+    longlat = meteostat.Point(
+        44.864812831248145, -0.5748540613726731
+        )
+
+    data = meteostat.Daily(loc=longlat, start=start, end=end)
+    data = data.fetch()
+    df = data[[x for x in data.columns if x in ['tavg', 'tmin', 'tmax', 'tsun', 'prcp']]]
+    df = df.stack().reset_index()
+    df = df.T
+    return df
+
+def make_weather_df(interval, winedata_path):
+    if interval == 'daily':
+        weather_columns = get_weather_daily('bordeaux', 2000)
+        df_weather = pd.DataFrame(columns=weather_columns.columns)
+            
+        df = pd.read_csv(winedata_path)
+        df = df.iloc[:,:4]
+
+        for index, row in df.iterrows():
+            weather = get_weather_daily('bordeaux',row['Year']).iloc[2,:]
+            df_weather = df_weather.append(weather)
+            print(df_weather.tail(2))
+            print(df_weather.shape)
+        df = df.join(df_weather)
+        return df 
+    elif interval == 'monthly':
+        weather_columns = get_weather_monthly('bordeaux', 2000)
+        df_weather = pd.DataFrame(columns=weather_columns.columns)
+            
+        df = pd.read_csv(winedata_path)
+        df = df.iloc[:,:4]
+
+        for index, row in df.iterrows():
+            weather = get_weather_monthly('bordeaux',row['Year']).iloc[2,:]
+            df_weather = df_weather.append(weather)
+            print(df_weather.tail(2))
+            print(df_weather.shape)
+        df = df.join(df_weather)
+        return df 
 
