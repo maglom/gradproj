e947752e01be33d24a92ca76869d77216379c25d
Taigahstaille <108455597+Taigahstaille@users.noreply.github.com>
2022-09-14 12:55:44 +0200

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,34 @@
+
+from flask import Flask, request, jsonify, render_template, url_for, redirect
+from sql.sqlfunc import query
+from tabulate import tabulate
+
+#wines = query('select distinct wine from viners order by wine asc')
+#winelist = [[''.join(x)] for x in wines]
+#years = query('select distinct years from viners order by years asc')
+#yearlist = [[''.join(x)] for x in years]
+
+#tablewine = tabulate(winelist, tablefmt='html')
+#tableyear = tabulate(yearlist, tablefmt='html')
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    wines = query('select distinct wine from viners order by wine asc')
+    years = query('select distinct years from viners order by years asc')
+    return render.template('wine.html', wines=wines, years=years)
+    
+    #if request.method == 'POST':
+    #    wine = request.form['wine']
+    #    year = request.form['year']
+    #    return redirect(url_for('result', wine=f'{wine} {year}'))
+    #else:
+    #    return render_template('start.html') + tablewine + tableyear
+
+@app.route('/<wine>')
+def result(wine):
+    return f'<h1>{wine}</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
