fa7fb6dd92c9f878810f4d94123cacf1d270c282
Maglom <magnuslomsdalen@gmail.com>
2022-09-16 10:59:48 +0200

    webapp

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
-from flask import Flask, request, jsonify, render_template, url_for, redirect
+from flask import Flask, flash, redirect, render_template, request, url_for, redirect
 from sql.sqlfunc import query
-from tabulate import tabulate
+
 
 wines = query('select distinct wine from viners order by wine asc')
-winelist = [[''.join(x)] for x in wines]
 years = query('select distinct years from viners order by years asc')
-yearlist = [[''.join(x)] for x in years]
 
-tablewine = tabulate(winelist, tablefmt='html')
-tableyear = tabulate(yearlist, tablefmt='html')
+winelist = [''.join(x) for x in wines]
+yearlist = [''.join(x) for x in years]
+
+winedict = [{'name':x} for x in winelist]
+yeardict = [{'name':x} for x in yearlist]
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def home():
-    if request.method == 'POST':
-        wine = request.form['wine']
-        year = request.form['year']
-        return redirect(url_for('result', wine=f'{wine} {year}'))
-    else:
-        return render_template('start.html') + tablewine + tableyear
+@app.route('/')
+def index():
+    return render_template(
+        'index2.html', 
+        data = winedict,
+        data2 = yeardict)
 
-@app.route('/<wine>')
-def result(wine):
-    return f'<h1>{wine}</h1>'
+@app.route("/test", methods = ['GET', 'POST'])
+
+
+def test():
+    select1 = request.form.get('comp_select_y')
+    select2 = request.form.get('comp_select')
+    rating = query(f"select score from viners where wine = '{select2}' and years = '{select1}'")
+    rating = rating[0][0]
+    return f"<H1>Wine = {select2}<br> Vintage = {select1}<br> Rating = {rating}</H1>"
+        
+    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+   app.run(debug = True)
